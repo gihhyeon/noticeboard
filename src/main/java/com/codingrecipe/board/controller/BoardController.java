c3352a458080feea88a8da705496488006003721
gihhyeon <fineblue.kh@gmail.com>
2025-02-04 23:56:18 +0900

    04_게시글 작성화면 만들기

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -3,10 +3,16 @@ package com.codingrecipe.board.controller;
 import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
 }
