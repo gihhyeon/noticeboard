a873e189d16c915015f413b97f35039fc6b7fa76
gihhyeon <fineblue.kh@gmail.com>
2025-02-05 18:44:58 +0900

    07_게시글 데이터 DB 저장하기

--- src/main/java/com/codingrecipe/board/repository/BoardRepository.java
+++ src/main/java/com/codingrecipe/board/repository/BoardRepository.java
@@ -1,7 +1,17 @@
 package com.codingrecipe.board.repository;
 
+import com.codingrecipe.board.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BoardRepository {
+
+    private final SqlSessionTemplate sql;
+
+    public void save(BoardDTO boardDTO) {
+        sql.insert("Board.save", boardDTO); // Board는 maapr의 namespace를 가리킨다.
+    }
 }
